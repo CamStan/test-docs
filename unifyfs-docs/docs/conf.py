edec96948bd682f5473b25780e7a941e3755792c
CamStan <stanavige1@llnl.gov>
2023-10-23 18:12:43 -0700

    fix custom css file variable

--- unifyfs-docs/docs/conf.py
+++ unifyfs-docs/docs/conf.py
@@ -139,7 +139,7 @@ html_logo = 'images/UnifyFS-logo.png'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-def setup(app):   app.add_stylesheet("theme_overrides.css" )
+html_css_files = ['theme_overrides.css']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
